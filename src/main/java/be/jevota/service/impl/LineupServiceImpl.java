3a05278eee251af584296b115c9658d06c52a334
Peter Mesotten <p.mesotten@aca-it.be>
2013-10-05 16:50:13 +0200

    #32: Opstellingsmail bevat naam van home club i.p.v. naam van tegenstander.

--- src/main/java/be/jevota/service/impl/LineupServiceImpl.java
+++ src/main/java/be/jevota/service/impl/LineupServiceImpl.java
@@ -12,6 +12,7 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import be.jevota.domain.GameLineup;
+import be.jevota.domain.PingpongClub;
 import be.jevota.domain.PingpongGame;
 import be.jevota.domain.PingpongPlayer;
 import be.jevota.domain.PingpongTeam;
@@ -85,10 +86,11 @@ public class LineupServiceImpl implements LineupService {
 		Collection<PingpongPlayer> players = lineup.getPlayers();
 		Collection<PingpongPlayer> cc = roleService.getOrCreateRole(RoleName.LINEUP_MANAGER).getPlayers();
 		String title = "Ploegopstelling " + team.getName() + " - " + date;
+		PingpongClub homeClub = game.getHomeTeam().getClub();
 		String body = "Beste,\n\n" + "Jullie staan opgesteld voor de wedstrijd tegen " + lineup.getOpponent().getName()
 				+ ":\n- " + StringUtils.join(players, "\n- ") + "\n\nDeze wedstrijd wordt op " + date
-				+ " gespeeld op de volgende locatie:\n\n" + lineup.getOpponent().getName() + "\n"
-				+ game.getHomeTeam().getClub().getAddress().getFullAddress("\n") + "\n\n"
+				+ " gespeeld op de volgende locatie:\n\n" + homeClub.getName() + "\n"
+				+ homeClub.getAddress().getFullAddress("\n") + "\n\n"
 				+ "Graag een seintje als dit niet voor je lukt.\n\n" + "T.T.C. Jevota Lanaken";
 		mailService.sendEmail(players, cc, title, body, true);
 	}
